daa0e28cb4c80d4fc926a6171f665e1ef9b31e74
SofiiaHlushchak <glushchak02@gmail.com>
2024-08-26 11:27:34 +0200

    init commit

---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: "idle",
+    filters: [],
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "HEROES_FETCHING":
+            return {
+                ...state,
+                heroesLoadingStatus: "loading",
+            };
+        case "HEROES_FETCHED":
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: "idle",
+            };
+        case "HEROES_DELETE":
+            return {
+                ...state,
+                heroes: state.heroes.filter((item) => item.id !== action.id),
+                heroesLoadingStatus: "idle",
+            };
+        case "HEROES_FETCHING_ERROR":
+            return {
+                ...state,
+                heroesLoadingStatus: "error",
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
